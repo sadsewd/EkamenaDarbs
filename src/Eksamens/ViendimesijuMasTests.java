dcdd438fd3e4c740eb6cc74c61bfa0e50a8fbe1b
sadsewd <[lacitiskarlis@gmail.com]>
2021-06-11 11:22:46 +0300

    Pievienots programmas info un jautājums lietotājam par testa uzsākšanu (Pagājušais commit ir veikts neveiksmīgi)

--- src/Eksamens/ViendimesijuMasTests.java
+++ src/Eksamens/ViendimesijuMasTests.java
@@ -1,10 +1,31 @@
 package Eksamens;
 
+import java.util.Scanner;
+
 public class ViendimesijuMasTests {
 
-	public static void main(String[] args) {
-	int punkti;
+	public static void tests() {
+		
+		
+	}
 	
+	public static void main(String[] args) {
+		Scanner dati = new Scanner(System.in);
+		String pagString;
+		char atbilde;
+		
+		System.out.println("Labdien! Šī programma testēs jūsu zināšanas par viendimensiju masīviem!");
+
+		System.out.print("\nVai esat gatavs uzsākt testu (j - jā; n - nē): ");
+		pagString = dati.next();
+		atbilde = pagString.charAt(0);
+		dati.close();
+		
+		switch(atbilde) {	
+		case 'j' : tests(); break;
+		case 'n' : break;
+		default : System.out.println("Tika nepareizi ievadīta atbilde!");
+			}
 	}
 
 }
